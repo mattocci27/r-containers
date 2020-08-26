e634e57b042eafa110fdfb8c62be38f3ced45607
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-08-26 18:04:53 +0800

    use _ instead of -

--- docker_to_sing.py
+++ docker_to_sing.py
@@ -11,5 +11,5 @@ files = os.listdir(path=sdir)
 d = open('docker-compose.yml')
 d2 = yaml.safe_load(d)
 for k in d2['services'].keys():
-    subprocess.call([ 'echo', 'singularity', 'build', 'singularity/' + k + '.sif', 'docker-daemon://' + MY_IP + '/mattocci/' + k.replace('-', ':')])
-    subprocess.call([ 'sudo', 'singularity', 'build', 'singularity/' + k + '.sif', 'docker-daemon://' + MY_IP + '/mattocci/' + k.replace('-', ':')])
+    subprocess.call([ 'echo', 'singularity', 'build', 'singularity/' + k + '.sif', 'docker-daemon://' + MY_IP + '/mattocci/' + k.replace('_', ':')])
+    subprocess.call([ 'sudo', 'singularity', 'build', 'singularity/' + k + '.sif', 'docker-daemon://' + MY_IP + '/mattocci/' + k.replace('_', ':')])
