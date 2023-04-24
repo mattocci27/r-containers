9f7f0b36a2385368119ba5ceb5ff74c92e71f9f2
Masatoshi Katabuchi <mattocci27@gmail.com>
2023-04-24 15:16:43 +0900

    add deps

--- build_images.py
+++ build_images.py
@@ -1,11 +1,16 @@
 import yaml
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict
 
-def build_docker_image(service, build_info):
+dependencies = {
+    'radian-verse_4.3.0': 'radian_4.3.0'
+}
+
+def build_docker_image(service_info, build_info):
     context = build_info['context']
     dockerfile = build_info['dockerfile']
-    image = service['image']
+    image = service_info['image']
 
     cmd = f'docker build -t {image} -f {dockerfile} {context}'
     print(f'Building {image}...')
@@ -18,17 +23,38 @@ def build_docker_image(service, build_info):
         print(f'Failed to build {image}')
         print(process.stderr)
 
+def build_images_in_order(services):
+    # Perform topological sort on the dependency graph
+    graph = defaultdict(list)
+    visited = set()
+    build_order = []
+
+    for dependent, dependency in dependencies.items():
+        graph[dependency].append(dependent)
+
+    def visit(service_name):
+        if service_name not in visited:
+            visited.add(service_name)
+            for dependent in graph[service_name]:
+                visit(dependent)
+            build_order.append(service_name)
+
+    for service_name in services:
+        if service_name not in visited:
+            visit(service_name)
+
+    # Build images in the correct order
+    with ThreadPoolExecutor() as executor:
+        for service_name in build_order:
+            service_info = services[service_name]
+            executor.submit(build_docker_image, service_info, service_info['build']).result()
+
 def main():
     with open('docker-compose.yml', 'r') as file:
         compose_data = yaml.safe_load(file)
 
     services = compose_data['services']
-
-    with ThreadPoolExecutor() as executor:
-        futures = [
-            executor.submit(build_docker_image, service_info, service_info['build'])
-            for service_name, service_info in services.items()
-        ]
+    build_images_in_order(services)
 
 if __name__ == '__main__':
     main()
