996aac79a3e1b2bd4ea07abc834b45b2e3b653bc
Masatoshi Katabuchi <mattocci27@gmail.com>
2024-03-09 03:21:00 +0000

    test

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set up the environment
-poetry install
-docker login --username your-username --password your-password
+# poetry install
+# docker login --username your-username --password your-password
 
 # Run the Python scripts
-poetry run python scripts/update.py
-poetry run python scripts/generate_bake.py
+# poetry run python scripts/update.py
+# poetry run python scripts/generate_bake.py
 
 # Set up Docker Buildx
 docker buildx create --name mybuilder --use
@@ -14,5 +14,3 @@ docker buildx inspect --bootstrap
 
 # Build and push the images
 docker buildx bake -f test/bakefile_amd.json --push test
-# docker-compose -f docker-compose.yml build
-# docker-compose -f docker-compose.yml push
