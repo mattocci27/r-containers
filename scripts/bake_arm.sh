15a25bad3307e6fc80e0db0ca561e41b25c3ddf7
Masatoshi Katabuchi <mattocci27@gmail.com>
2025-09-16 09:57:53 +0000

    clean

--- scripts/bake_arm.sh
+++ scripts/bake_arm.sh
@@ -6,7 +6,7 @@ TARGET_BAKEFILE=${1:-bakefiles/bakefile_arm.json}
 
 echo "Using bakefile: ${TARGET_BAKEFILE}"
 
-for target in radian radian-verse radian-ai cmdstan-verse cmdstan-zsh cmdstan-ai; do
+for target in radian radian-verse radian-zsh radian-ai cmdstan-verse cmdstan-zsh cmdstan-ai; do
   echo "Building and pushing ${target}..."
   docker buildx bake -f "$TARGET_BAKEFILE" --push "$target"
 done
